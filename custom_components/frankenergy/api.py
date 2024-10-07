08e1fba906682a5af3443c63270c1bd6db2aa2c0
Aidan Copps <aidan@copps.co.nz>
2024-10-08 08:36:41 +1300

    extra logging

--- custom_components/frankenergy/api.py
+++ custom_components/frankenergy/api.py
@@ -10,19 +10,19 @@ from urllib.parse import parse_qs
 
 _LOGGER = logging.getLogger(__name__)
 
-# http.client.HTTPConnection.debuglevel = 2
-# logging.basicConfig()
+logging.basicConfig()
 # logging.getLogger().setLevel(logging.DEBUG)
-# requests_log = logging.getLogger("requests.packages.urllib3")
-# requests_log.setLevel(logging.DEBUG)
-# requests_log.propagate = True
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
+
 
 class FrankEnergyApi:
     """Define the Frank Energy API."""
 
     def __init__(self, email, password):
         """Initialise the API."""
-        _LOGGER.warning("__init__")
+        _LOGGER.debug("__init__", exc_info=True)
         self._client_id = "9b63be56-54d0-4706-bfb5-69707d4f4f89"
         self._redirect_uri = 'eol://oauth/redirect',
         self._url_token_base = "https://energyonlineb2cprod.b2clogin.com/energyonlineb2cprod.onmicrosoft.com"
@@ -43,13 +43,14 @@ class FrankEnergyApi:
         for line in page.splitlines():
             if line.startswith("var SETTINGS = ") and line.endswith(";"):
                 # Remove the prefix and suffix to get valid JSON
-                json_string = line.removeprefix("var SETTINGS = ").removesuffix(";")
+                json_string = line.removeprefix(
+                    "var SETTINGS = ").removesuffix(";")
                 return json.loads(json_string)
         return None
 
     async def get_refresh_token(self):
         """Get the refresh token."""
-        _LOGGER.debug("API get_refresh_token")
+        _LOGGER.debug("API get_refresh_token", exc_info=True)
 
         async with aiohttp.ClientSession() as session:
             url = f"{self._url_token_base}/oauth2/v2.0/authorize"
@@ -63,7 +64,7 @@ class FrankEnergyApi:
                 'redirect_uri': 'eol://oauth/redirect',
             }
 
-            _LOGGER.debug("Step: 1")
+            _LOGGER.debug("Step: 1", exc_info=True)
             async with session.get(url, params=params) as response:
                 response_text = await response.text()
 
@@ -71,7 +72,8 @@ class FrankEnergyApi:
             trans_id = settings_json.get("transId")
             csrf = settings_json.get("csrf")
 
-            url = f"{self._url_token_base}/{self._p}/SelfAsserted?tx={trans_id}&p={self._p}"
+            url = f"{
+                self._url_token_base}/{self._p}/SelfAsserted?tx={trans_id}&p={self._p}"
             payload = {
                 "request_type": "RESPONSE",
                 "email": self._email,
@@ -79,11 +81,10 @@ class FrankEnergyApi:
             headers = {
                 'X-CSRF-TOKEN': csrf,
             }
-            _LOGGER.debug("Step: 2")
+            _LOGGER.debug("Step: 2", exc_info=True)
             async with session.post(url, headers=headers, data=payload) as response:
                 response_text = await response.text()
                 pass
-
 
             url = f"{self._url_token_base}/{self._p}/api/SelfAsserted/confirmed"
             params = {
@@ -91,7 +92,7 @@ class FrankEnergyApi:
                 'tx': trans_id,
                 'p': self._p,
             }
-            _LOGGER.debug("Step: 3")
+            _LOGGER.debug("Step: 3", exc_info=True)
             async with session.get(url, params=params) as response:
                 response_text = await response.text()
                 # Extract the new CSRF token from cookies because it changes here
@@ -108,12 +109,14 @@ class FrankEnergyApi:
                 'X-CSRF-TOKEN': csrf,
             }
 
-            url = f"{self._url_token_base}/{self._p}/SelfAsserted?tx={trans_id}&p={self._p}"
-            _LOGGER.debug("Step: 4")
+            url = f"{
+                self._url_token_base}/{self._p}/SelfAsserted?tx={trans_id}&p={self._p}"
+            _LOGGER.debug("Step: 4", exc_info=True)
             async with session.post(url, headers=headers, data=payload) as response:
                 pass
 
-            url = f"{self._url_token_base}/{self._p}/api/CombinedSigninAndSignup/confirmed"
+            url = f"{
+                self._url_token_base}/{self._p}/api/CombinedSigninAndSignup/confirmed"
             params = {
                 'rememberMe': 'false',
                 'csrf_token': csrf,
@@ -121,7 +124,7 @@ class FrankEnergyApi:
                 'p': self._p
             }
             headers = {}
-            _LOGGER.debug("Step: 5")
+            _LOGGER.debug("Step: 5", exc_info=True)
             async with session.get(url, headers=headers, params=params, allow_redirects=False) as response:
                 response.raise_for_status()
                 response_data = await response.text()
@@ -132,7 +135,8 @@ class FrankEnergyApi:
                     error = query_params['error'][0]
                     _LOGGER.error("Error in response: %s", error)
                     error_description = query_params['error_description'][0]
-                    _LOGGER.error("Error description in response: %s", error_description)
+                    _LOGGER.error(
+                        "Error description in response: %s", error_description)
 
             code = query_params['code'][0]
             url = f"{self._url_token_base}/{self._p}/oauth2/v2.0/token"
@@ -150,14 +154,16 @@ class FrankEnergyApi:
                 response_data = await response.json()
                 refresh_token = response_data.get('refresh_token')
                 access_token = response_data.get('access_token')
-                refresh_token_expires_in = response_data.get('refresh_token_expires_in')
+                refresh_token_expires_in = response_data.get(
+                    'refresh_token_expires_in')
                 access_token_expires_in = response_data.get('expires_in')
 
             self._token = access_token
             self._refresh_token = refresh_token
             self._refresh_token_expires_in = refresh_token_expires_in
             self._access_token_expires_in = access_token_expires_in
-            _LOGGER.debug("Refresh token retrieved successfully")
+            _LOGGER.debug(
+                "Refresh token retrieved successfully", exc_info=True)
 
     async def get_api_token(self):
         """Get token from the Frank Energy API."""
@@ -174,22 +180,22 @@ class FrankEnergyApi:
                 if response.status == 200:
                     jsonResult = await response.json()
                     self._token = jsonResult["access_token"]
-                    _LOGGER.debug(f"Auth Token: {self._token}")
+                    _LOGGER.debug(f"Auth Token: {self._token}", exc_info=True)
                 else:
-                    _LOGGER.error("Failed to retrieve the token page.")
-
+                    _LOGGER.error(
+                        "Failed to retrieve the token page.", exc_info=True)
 
     async def get_data(self):
         """Get data from the API."""
 
         access_token_threshold = timedelta(minutes=5).total_seconds()
         if self._access_token_expires_in <= access_token_threshold:
-            _LOGGER.warning("Access token needs renewing")
+            _LOGGER.warning("Access token needs renewing", exc_info=True)
             await self.get_api_token()
 
         refresh_token_threshold = timedelta(minutes=5).total_seconds()
         if self._refresh_token_expires_in <= refresh_token_threshold:
-            _LOGGER.warning("Refresh token needs renewing")
+            _LOGGER.warning("Refresh token needs renewing", exc_info=True)
             await self.get_refresh_token()
 
         headers = {
@@ -203,9 +209,10 @@ class FrankEnergyApi:
         # If you want to query more than ~4 days of hourly data, at a time you need to split it into multiple requests
         # or the the latest usage will not be returned.
         to_date = datetime.now()
-        from_date = to_date - timedelta(days=4) # fetch 4 days worth of data
+        from_date = to_date - timedelta(days=4)  # fetch 4 days worth of data
 
-        url = f"{self._url_data_base}/v2/private/usage/electricity/aggregatedSiteUsage/hourly"
+        url = f"{
+            self._url_data_base}/v2/private/usage/electricity/aggregatedSiteUsage/hourly"
         params = {
             'startDate': from_date.strftime("%Y-%m-%d"),
             'endDate': to_date.strftime("%Y-%m-%d"),
@@ -217,8 +224,10 @@ class FrankEnergyApi:
                 data = await response.json()
                 # _LOGGER.debug(f"get_data returned data: {data}")
                 if not data:
-                    _LOGGER.warning("Fetched consumption successfully but there was no data")
+                    _LOGGER.warning(
+                        "Fetched consumption successfully but there was no data", exc_info=True)
                 return data
             else:
-                _LOGGER.error("Could not fetch consumption")
+                _LOGGER.error(
+                    "Could not fetch consumption. Response: %s", response, exc_info=True)
                 return None
